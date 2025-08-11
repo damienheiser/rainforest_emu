6a79cfafc1ef21b7518adfda817de6bfae926ad8
Damien Heiser <damienheiser@users.noreply.github.com>
2025-08-11 13:52:08 -0700

    Make integration loadable by Home Assistant

--- const.py
+++ const.py
@@ -1,4 +1,10 @@
-"""Constants for the Rainforest RAVEn integration."""
+"""Constants for the Rainforest EMU2 integration."""
 
-DEFAULT_NAME = "Rainforest RAVEn"
-DOMAIN = "rainforest_raven"
+DEFAULT_NAME = "Rainforest EMU2"
+DOMAIN = "rainforest_emu"
+
+# Configuration keys
+CONF_DEVICE_MAC = "device_mac"
+
+# Defaults
+DEFAULT_DEVICE_MAC = "00:00:00:00:00:00"
